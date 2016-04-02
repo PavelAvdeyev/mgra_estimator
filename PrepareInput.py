ad441287400a8d30a663d6e142e453a214971b54
StanKozlov <stanbiology@gmail.com>
2016-04-02 23:40:31 +0300

    Working version for GASTS, PMAG+, MGRA and GapAdj

--- PrepareInput.py
+++ PrepareInput.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3.4
 
 import os 
 import os.path
 import sys
 import logging
-import shutil
 
 import utils.utils as utils
-import PrepareInput.Writer as writer
-import PrepareInput.Converter as converter
 
-from parsers import Handler
+from parsers import Handler, PMAG_PLUS_handler, GapAdj_handler, MGRA_handler
+from parsers.GASTS_handler import GASTS_handler
 
 
 def prepare_input_files(dir_path, tools):
@@ -26,15 +24,21 @@ def prepare_input_files(dir_path, tools):
     genomes = Handler.parse_genomes_in_grimm_file(genomes_file)
     ancestors = Handler.parse_genomes_in_grimm_file(ancestors_file)
 
-    for tool in tools:
-        tool(dir_path, tree_file, bad_tree_file, genomes, ancestors)
-
 
 def dojob(name_directory, tools):
-    for path, name in utils.get_immediate_subdirectories(name_directory):
-        for subpath, subname in utils.get_immediate_subdirectories(path):
-            prepare_input_files(subpath, tools)
-            #prepare_ancestor(subpath)
+    for tool in tools:
+        for path, name in utils.get_immediate_subdirectories(name_directory):
+            for subpath, subname in utils.get_immediate_subdirectories(path):
+                if tool == 'GASTS':
+                    GASTS_handler().save(subpath)
+                elif tool == 'MGRA':
+                    MGRA_handler.MGRA_handler().save(subpath)
+                elif tool == 'GapAdj':
+                    GapAdj_handler.GapAdj_handler().save(subpath)
+                elif tool == 'PMAG+':
+                    PMAG_PLUS_handler.PMAG_PLUS_handler().save(subpath)
+                #prepare_input_files(subpath, tools)
+                #prepare_ancestor(subpath)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
@@ -49,12 +53,7 @@ if __name__ == "__main__":
 
     dir_path = os.path.abspath(sys.argv[1])
 
-    tools = [
-        converter.save_for_MGRA,
-        converter.save_for_PMAG,
-        converter.save_for_GapAdj,
-        converter.save_for_GASTS
-    ]
+    tools = ['GASTS', 'PMAG+', 'MGRA', 'GapAdj']
 
     dojob(dir_path, tools)
     #prepare_input_files(dir_path, tools)
