ab636c880501e27d563e66976e7d6437332bbc5c
StanKozlov <stanbiology@gmail.com>
2016-05-15 17:34:04 +0300

    Added procars and infercarspro part

--- measures/Comparator.py
+++ measures/Comparator.py
@@ -1,4 +1,4 @@
-from parsers import PMAG_PLUS_handler, Handler, GASTS_handler, GapAdj_handler, MGRA_handler
+from parsers import PMAG_PLUS_handler, Handler, GASTS_handler, GapAdj_handler, MGRA_handler, Procars_handler, infercarspro_handler
 import os
 import csv
 
@@ -20,10 +20,15 @@ def single_tool_data(tool, path):
         #     run_GapAdj(path, test_dir)
         # elif tool == 'MGRA':
         #     run_MGRA(path, test_dir)
-        # elif tool == 'Procars':
-        #     run_procars(path, test_dir)
-        # elif tool == 'InferCarsPro':
-        #     run_infercarspro(path, test_dir)
+        elif tool == 'Procars':
+            dist.append(Procars_handler.Procars_handler().compare_dist_procars(path + '/' + test_dir))
+            accuracy.append(Procars_handler.Procars_handler().compare_acc_procars(path + '/' + test_dir))
+            test.append(test_dir)
+        elif tool == 'InferCarsPro':
+            dist.append(infercarspro_handler.Infercarspro_handler().compare_dist_Infercarspro(path + '/' + test_dir))
+            accuracy.append(infercarspro_handler.Infercarspro_handler().compare_acc_Infercarspro(path + '/' + test_dir))
+            test.append(test_dir)
+
     mean_dist = sum(dist)/10
     TP, FN, FP = [], [], []
     print(accuracy)
@@ -41,13 +46,18 @@ def tool_compare_dist(tool, path):
     param_dirs = []
     dist = []
     for param_dir in os.listdir(path):
-        param_dirs.append(param_dir)
-        dist.append(single_tool_data(tool, path + '/' + param_dir)[0])
+        try:
+            dist.append(single_tool_data(tool, path + '/' + param_dir)[0])
+            param_dirs.append(param_dir)
+        except:
+            print('not done')
     return(list(zip(param_dirs, dist)))
 
 distances = tool_compare_dist('GASTS', '/home/hamster/noindel-sim')
-with open('distances.csv','w') as out:
+#distances = single_tool_data('Procars', '/home/hamster/noindel-sim/6_200_100_0')
+with open('distances_gasts.csv','w') as out:
     csv_out = csv.writer(out)
     csv_out.writerow(['name','DCJ distance'])
     for row in distances:
         csv_out.writerow(row)
+
